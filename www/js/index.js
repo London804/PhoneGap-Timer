abe72da2e53a6a607c24a51fc504b8e2932b7fd5
Alex <alexchaparro@Alexs-MacBook-Pro.local>
2015-02-18 12:51:57 -0500

    working on the timer

--- www/js/index.js
+++ www/js/index.js
@@ -48,11 +48,136 @@ var app = {
     }
 };
 
+app.initialize();
+
 $(function(){
    $('#login').click(function(){
        $('#welcome').toggle();
    });
 
+    ////global timeout references we can use to stop them
+    //var timeouts = {};
+    //
+    //
+    ////timer demo function with normal/self-adjusting argument
+    //function timer() {
+    //    //create the timer speed, a counter and a starting timestamp
+    //    //var speed = 50,
+    //    //    counter = 0,
+    //    //    start = new Date().getTime();
+    //
+    //    //timer instance function
+    //    function instance() {
+    //
+    //    }
+    //
+    //}
+    ////bind a submit handler to the normal form
+    //$('#submit').click(function(){
+    //    //call the timerdemo function with no flags
+    //    timer(this, false, false);
+    //
+    //    //cancel the normal submit
+    //    return false;
+    //
+    //});
+    //
+    ////bind a reset handler to stop it
+    //
+    //
+    //    $('#reset').click(function(){
+    //        //stop the form's timer
+    //        window.clearTimeout(timeouts[this.id]);
+    //
+    //        //cancel the normal reset
+    //        return false;
+    //
+    //    });
+
+    var Stopwatch = function(elem, options) {
+
+        var timer       = createTimer(),
+            startButton = $('#submit',start),
+            stopButton  = $('#stop', stop),
+            resetButton = $('#reset', reset),
+            offset,
+            clock,
+            interval;
+
+        // default options
+        options = options || {};
+        options.delay = options.delay || 1;
+
+
+        // initialize
+        reset();
+
+        // private functions
+        function createTimer() {
+        }
+
+        function createButton(action, handler) {
+            $('#submit').click(function(event) {
+                handler();
+                event.preventDefault();
+            });
+            return;
+        }
+
+        function start() {
+            if (!interval) {
+                offset   = Date.now();
+                interval = setInterval(update, options.delay);
+            }
+        }
+
+        function stop() {
+            if (interval) {
+                clearInterval(interval);
+                interval = null;
+            }
+        }
+
+        function reset() {
+            clock = 0;
+            render(0);
+        }
+
+        function update() {
+            clock += delta();
+            render();
+        }
+
+        function render() {
+            //timer.innerHTML = clock/1000;
+        }
+
+        function delta() {
+            var now = Date.now(),
+                d   = now - offset;
+
+            offset = now;
+            return d;
+        }
+
+        // public API
+        this.start  = start;
+        this.stop   = stop;
+        this.reset  = reset;
+    };
+
+
+// basic examples
+    var elems = document.getElementsByClassName("basic");
+
+    for (var i=0, len=elems.length; i<len; i++) {
+        new Stopwatch(elems[i]);
+    }
+
+
+
+
 });
 
-app.initialize();
+
+
